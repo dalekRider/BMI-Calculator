77d06146cedf41c78d2557382109351535423fe2
Johnathan Mercier <jmercier76@gmail.com>
2017-10-15 22:28:10 -0400

    Add files via upload

---
+++ bmiCalc.js
@@ -0,0 +1,40 @@
+// Calculate Body Mass Index (BMI)
+// English formula is
+  // BMI = (Weight in Pounds / (Height in inches x Height in inches)) x 703
+// Metric formula is
+  //BMI = (Weight in Kilograms / (Height in Meters x Height in Meters))
+
+const calculateBMI = function() {
+  //Get Weight and store as variable
+  let weight = prompt("what is your weight in lbs?");
+  //Get height and store as variable
+  let height = prompt("what is your height in inches?");
+  //English Formula solution
+  bmiEnglish = function (weight, height) {
+    let engWeight = weight;
+    let engHeight = (height * height);
+    //window.alert(((engWeight / engHeight)*703).toFixed(2));
+    let bmIndex = ((engWeight / engHeight)*703).toFixed(2);
+    if (bmIndex <= 18.5) {
+      window.alert('Your BMI index is: ' + bmIndex + '. Which is considered to be underweight.');
+    } else if (bmIndex <= 25.0) {
+      window.alert('Your BMI index is: ' + bmIndex + '. Which is considered normal.');
+    } else if (bmIndex <= 30.0) {
+      window.alert('Your BMI index is: ' + bmIndex + '. Which is considered to be overweight.');
+    } else if (bmIndex > 30.0) {
+      window.alert('Your BMI index is: ' + bmIndex + '. Which is considered to be obese.');
+    } else if (bmIndex !== NaN) {
+      window.alert('There seems to be an error. Please verify your information and try again');
+    }
+  };
+  bmiEnglish(weight, height);
+};
+
+//Determine BMI Classification
+//if bmi <= 18.5 return "Underweight"
+
+//if bmi <= 25.0 return "Normal"
+
+//if bmi <= 30.0 return "Overweight"
+
+//if bmi > 30 return "Obese"
